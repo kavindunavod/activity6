5d657d27e636c0bfc0bf40fc2018fa42ef0f209f
kavindunavod <140165157+kavindunavod@users.noreply.github.com>
2024-07-28 01:03:33 +0530

    Update activity6_2.scala

--- activity6_2.scala
+++ activity6_2.scala
@@ -1,18 +1,12 @@
-object activity6_2 {
+object Activity6_2 {
 
   // Function to validate input
   def validateInput(name: String, marks: Int, totalMarks: Int): (Boolean, Option[String]) = {
-    if (name.isEmpty) {
-      (false, Some("Name cannot be empty."))
-    } else if (marks < 0) {
-      (false, Some("Marks cannot be negative."))
-    } else if (totalMarks <= 0) {
-      (false, Some("Total possible marks must be a positive number."))
-    } else if (marks > totalMarks) {
-      (false, Some("Marks cannot exceed total possible marks."))
-    } else {
-      (true, None)
-    }
+    if (name.isEmpty) (false, Some("Name cannot be empty."))
+    else if (marks < 0) (false, Some("Marks cannot be negative."))
+    else if (totalMarks <= 0) (false, Some("Total possible marks must be a positive number."))
+    else if (marks > totalMarks) (false, Some("Marks cannot exceed total possible marks."))
+    else (true, None)
   }
 
   // Function to read student info
@@ -26,28 +20,25 @@ object activity6_2 {
     println("Enter total possible marks:")
     val totalMarks = scala.io.StdIn.readInt()
 
-    val (isValid, errorMessage) = validateInput(name, marks, totalMarks)
-
-    if (!isValid) {
-      println(errorMessage.getOrElse("Invalid input."))
-      return getStudentInfo()  // Retry if input is invalid
+    validateInput(name, marks, totalMarks) match {
+      case (true, _) =>
+        val percentage = (marks.toDouble / totalMarks) * 100
+        val grade = percentage match {
+          case p if p >= 90 => 'A'
+          case p if p >= 75 => 'B'
+          case p if p >= 50 => 'C'
+          case _ => 'D'
+        }
+        (name, marks, totalMarks, percentage, grade)
+      case (_, Some(errorMessage)) =>
+        println(errorMessage)
+        getStudentInfo() // Retry if input is invalid
     }
-
-    val percentage = (marks.toDouble / totalMarks) * 100
-    val grade = percentage match {
-      case p if p >= 90 => 'A'
-      case p if p >= 75 => 'B'
-      case p if p >= 50 => 'C'
-      case _ => 'D'
-    }
-
-    (name, marks, totalMarks, percentage, grade)
   }
 
   // Function to print student record
-  def printStudentRecord(studentRecord: (String, Int, Int, Double, Char)): Unit = {
-    val (name, marks, totalMarks, percentage, grade) = studentRecord
-
+  def printStudentRecord(record: (String, Int, Int, Double, Char)): Unit = {
+    val (name, marks, totalMarks, percentage, grade) = record
     println(s"Student Name: $name")
     println(s"Marks: $marks")
     println(s"Total Possible Marks: $totalMarks")
@@ -56,36 +47,12 @@ object activity6_2 {
     println("-----------")
   }
 
-  // Function to get student info with retry
-  def getStudentInfoWithRetry(): (String, Int, Int, Double, Char) = {
-    var isValid = false
-    var studentRecord: (String, Int, Int, Double, Char) = ("", 0, 0, 0.0, 'D')
-
-    while (!isValid) {
-      studentRecord = getStudentInfo()
-      val (name, marks, totalMarks, percentage, grade) = studentRecord
-      val validation = validateInput(name, marks, totalMarks)
-
-      if (validation._1) {
-        isValid = true
-      } else {
-        println(validation._2.getOrElse("Invalid input. Please try again."))
-      }
-    }
-    studentRecord
-  }
-
   // Function to get number of students and their records
   def getAllStudentRecords(numStudents: Int): List[(String, Int, Int, Double, Char)] = {
-    var studentRecords: List[(String, Int, Int, Double, Char)] = List()
-
-    for (_ <- 1 to numStudents) {
-      println(s"Enter details for student ${studentRecords.size + 1}:")
-      val studentRecord = getStudentInfoWithRetry()
-      studentRecords = studentRecords :+ studentRecord
+    (1 to numStudents).toList.map { i =>
+      println(s"Enter details for student $i:")
+      getStudentInfo()
     }
-
-    studentRecords
   }
 
   def main(args: Array[String]): Unit = {
